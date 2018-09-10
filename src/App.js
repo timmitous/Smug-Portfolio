1772992cb548fce417291c07c902fbac4ce29047
Tim Wackenfors <timmitous@gmail.com>
2018-09-10 19:20:47 +0200

    Changed to different api task

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+/*import logo from './logo.svg';*/
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+    componentDidMount() {
+      console.log('Tryna fetch')
+      axios.get('https://jsonplaceholder.typicode.com/posts')
+      .catch(error => console.log('Unable to receive data', error))
+      .then(response => console.log('Received data', response.data[0]));
+     
+      }
+
   render() {
     return (
       <div className="App">
@@ -20,3 +29,55 @@ class App extends Component {
   }
 }
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Used for fetch api
+ /*fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(json => console.log(json[0]))*/
+
+
+/*{
+          console.log('Fugg man')
+          console.log(results);
+          console.log('redux')
+          console.log(results.json());
+          return results;
+        }).then(data => {
+          console.log(data)
+          return data;
+          //let whatever = data.results.map((url))
+      }*/
+    
+      
+      /* fetch('http://modernacodechallenge.azurewebsites.net/api/insurances',{mode:'no-cors'})
+      .then(response => console.log(response))
+      .then(json => console.log(json))*/
