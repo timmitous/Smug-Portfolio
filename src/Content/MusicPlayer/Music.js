1c7fd7d57654dc43f3fc67a31980e519c8d2247d
Tim Wackenfors <timmitous@gmail.com>
2018-12-13 22:43:14 +0100

    Fixed the player to almost 100% functioning. All that really remains is fixing an audio levels control and also a time control to skip through time... And also a replay button of course. But before that it would be fun to make a database through mongo to handle songs, information etc.

--- src/Content/MusicPlayer/Music.js
+++ src/Content/MusicPlayer/Music.js
@@ -1,18 +1,7 @@
 import React from "react";
-import paul from "../../images/paulie.jpg";
-import ole from "../../music/01-Ole.mp3";
 import ThePlayer from "./Player.js";
 import "./Music.css";
 import "./NewStyles.scss";
-import {
-  Card,
-  CardImg,
-  CardText,
-  CardBody,
-  CardTitle,
-  CardSubtitle,
-  Col
-} from "reactstrap";
 
 console.log("Logging from Music");
 
@@ -24,19 +13,6 @@ const Musics = () => {
       <div className="Container">
         <TheTitle />
         <ThePlayer />
-        <Col sm="4" key="1">
-          <Card>
-            <CardImg top width="100%" src={paul} alt="Card image cap" />
-            <CardBody>
-              <CardTitle>Paul Burnley</CardTitle>
-              <CardSubtitle>Enjoy</CardSubtitle>
-              <CardText>Paul Burnley and the Fourth Kingdom</CardText>
-              <a href={ole} download>
-                Get it on your device here mate!
-              </a>
-            </CardBody>
-          </Card>
-        </Col>
       </div>
     </div>
   );
--- src/Content/MusicPlayer/Player.js
+++ src/Content/MusicPlayer/Player.js
@@ -1,81 +1,116 @@
-import React from "react";
+import React, { Component } from "react";
 import "./NewStyles.scss";
 import ole from "../../music/01-Ole.mp3";
-//import paul from "../../images/paulie.jpg";
+import paul from "../../images/paulie.jpg";
 
 console.log("Logging from Player");
 
-class MusicPlayer extends React.Component {
+class MusicPlayer extends Component {
   render() {
     return <Player />;
   }
 }
 
 // Player
-class Player extends React.Component {
+class Player extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      name: "We Were Young",
-      artist: "Odesza",
-      album: "Summer's Gone",
-      year: 2012,
-      artwork:
-        "https://funkadelphia.files.wordpress.com/2012/09/odesza-summers-gone-lp.jpg",
-      duration: 192,
-      source: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/wwy.mp3"
+    this.audio = new Audio(ole);
+
+    this.infos = {
+      name: "Ole",
+      artist: "Paul Burnley",
+      album: "Save the white race",
+      year: 1992,
+      artwork: paul,
+      duration: 226,
+      source: ole
     };
 
-    this.play = {
-      playStatus: "play",
+    this.state = {
+      isPlaying: true,
       currentTime: 0
     };
+
+    this.togglePlay = this.togglePlay.bind(this);
+  }
+
+  togglePlay() {
+    this.setState({ isPlaying: !this.state.isPlaying });
+    console.log(this.state, "thisplay");
+    console.log(this.state.isPlaying, "isplayin");
+    this.whenPlaying();
+  }
+
+  whenPlaying() {
+    if (this.state.isPlaying) {
+      this.audio.play();
+
+      this.intervalId = setInterval(() => {
+        let currentTime = this.audio.currentTime;
+        let duration = this.infos.duration;
+
+        // Calculate percent of song
+        let percent = (currentTime / duration) * 100 + "%";
+        this.updateScrubber(percent);
+        this.updateTime(currentTime);
+      }, 100);
+    } else {
+      this.audio.pause();
+      clearInterval(this.intervalId);
+    }
+  }
+
+  updateScrubber(percent) {
+    let innerScrubber = document.querySelector(".Scrubber-Progress");
+    innerScrubber.style["width"] = percent;
+  }
+
+  updateTime(timestamp) {
+    timestamp = Math.floor(timestamp);
+    console.log(timestamp, "timestamp");
+    this.setState({ currentTime: timestamp });
   }
 
   render() {
-    console.log(this.state, "Tracks");
-    console.log(this.play.currentTime, "currenttime");
-    //works
     return (
       <div className="Player">
         <div
           className="Background"
-          style={{ backgroundImage: "url(" + this.state.artwork + ")" }}
+          style={{ backgroundImage: "url(" + this.infos.artwork + ")" }}
         />
         <div className="Header">
           <div className="Title">Now playing</div>
         </div>
         <div
           className="Artwork"
-          style={{ backgroundImage: "url(" + this.state.artwork + ")" }}
+          style={{ backgroundImage: "url(" + this.infos.artwork + ")" }}
         />
+
         <TrackInformation
-          trackName={this.state.name}
-          artistName={this.state.artist}
-          albumName={this.state.album}
+          trackName={this.infos.name}
+          artistName={this.infos.artist}
+          albumName={this.infos.album}
         />
         <Scrubber />
-        <Controls />
-        <Timestamps
-          duration={this.state.duration}
-          currentTime={this.play.currentTime}
+        <Controls isPlaying={this.state.playStatus} onClick={this.togglePlay} />
+        <TimeStamps
+          duration={this.infos.duration}
+          currentTime={this.state.currentTime}
         />
-        <audio>
-          <source src={ole} />
-        </audio>
       </div>
     );
   }
 }
 
-const TrackInformation = () => {
+const TrackInformation = props => {
   //works
   return (
     <div className="TrackInformation">
-      <div className="Name">We Were Young</div>
-      <div className="Artist">Odesza</div>
-      <div className="Album">Summer's Gone (2012)</div>
+      <div className="Name">{props.trackName}</div>
+      <div className="Artist">{props.artistName}</div>
+      <div className="Album">{props.albumName}</div>
     </div>
   );
 };
@@ -89,172 +124,50 @@ const Scrubber = () => {
   );
 };
 
-const Controls = () => {
-  //works
-  return (
-    <div className="Controls">
-      <div className="Button">
-        <i className="fa fa-fw fa-play" />
-      </div>
-    </div>
-  );
-};
-
-class Timestamps extends React.Component {
+class Controls extends Component {
   render() {
-    console.log(this.props, "should display duration and currenttime");
+    let classNames;
+    if (this.props.isPlaying === false) {
+      classNames = "fa f a-fw fa-pause";
+    } else {
+      classNames = "fa fa-fw fa-play";
+    }
+
     return (
-      <div className="Timestamps">
-        <div className="Time Time--current">{this.props.currentTime}</div>
-        <div className="Time Time--total">{this.props.duration}</div>
+      <div className="Controls">
+        <div className="Button" onClick={this.props.onClick}>
+          <i className={classNames} />
+        </div>
       </div>
     );
   }
 }
 
+class TimeStamps extends Component {
+  render() {
+    //console.log(this.props, "should display duration and currenttime");
+    return (
+      <div className="Timestamps">
+        <div className="Time Time--current">
+          {ConvertTime(this.props.currentTime)}
+        </div>
+        <div className="Time Time--total">
+          {ConvertTime(this.props.duration)}
+        </div>
+      </div>
+    );
+  }
+}
+
+const ConvertTime = props => {
+  let minutes = Math.floor(props / 60);
+  let seconds = props - minutes * 60;
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+  let timestamp = minutes + ":" + seconds;
+  //console.log(props, "timestampening");
+  return timestamp;
+};
+
 export default MusicPlayer;
-
-//CSS code
-
-/*.Player {
-  background: bisque;
-  border-radius: 5px;
-  overflow: hidden;
-  box-shadow: 0 5px 10px -5px black;
-  height: 667px;
-  position: relative;
-  width: 375px;
-  margin: auto;
-  margin-bottom: 3em;
-}
-
-.Background {
-  width: 150%;
-  height: 150%;
-  position: absolute;
-  top: -25%;
-  left: -25%;
-  background: url(https://funkadelphia.files.wordpress.com/2012/09/odesza-summers-gone-lp.jpg);
-  background-size: cover;
-  background-position: center center;
-  opacity: 0.2;
-  filter: blur(10px);
-}
-
-.Title {
-  width: 300px;
-  margin: 50px auto;
-  font-size: 12px;
-  text-transform: uppercase;
-  letter-spacing: 0.1em;
-  position: relative;
-}
-
-.Artwork {
-  width: 300px;
-  height: 300px;
-  background: url(https://funkadelphia.files.wordpress.com/2012/09/odesza-summers-gone-lp.jpg);
-  background-size: cover;
-  background-position: center center;
-  border-radius: 4px;
-  margin: auto;
-  box-shadow: 0 5px 10px -5px black;
-  position: relative;
-}
-
-.TrackInformation {
-  width: 300px;
-  margin: 30px auto;
-  text-align: center;
-  position: relative;
-}
-
-.Name {
-  font-size: 24px;
-  margin-bottom: 10px;
-  font-weight: 300;
-}
-
-.Artist {
-  font-size: 16px;
-  margin-bottom: 5px;
-  font-weight: 500;
-}
-
-.Album {
-  font-size: 12px;
-  opacity: 0.5;
-}
-
-.Scrubber {
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 20%;
-  opacity: 0.2;
-  transition: opacity 0.25s ease;
-  border-bottom-left-radius: 5px;
-  border-bottom-right-radius: 5px;
-}
-
-.Scrubber:active {
-  opacity: 1;
-}
-
-.Scrubber-Progress {
-  height: 100%;
-  width: 0%;
-  transition: width 0.25s ease;
-  border-bottom-left-radius: 5px;
-  border-bottom-right-radius: 5px;
-}
-
-.Timestamps {
-  display: flex;
-  justify-content: space-between;
-  box-sizing: border-box;
-  padding: 20px;
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  pointer-events: none;
-  z-index: 2;
-  width: 100%;
-}
-
-.Time {
-  font-size: 12px;
-}
-
-.Controls {
-  position: absolute;
-  bottom: 8%;
-  pointer-events: none;
-  margin: auto;
-  left: 0;
-  right: 0;
-}
-
-.Button {
-  height: 60px;
-  width: 60px;
-  border: 2px solid white;
-  box-sizing: border-box;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 30px;
-  box-shadow: 0 5px 5px 0px black;
-  margin: auto;
-  pointer-events: all;
-}
-.Button:active {
-  transform: scale(0.98);
-  background: white;
-}
-
-.fa-play:before {
-  content: "\f04b";
-}
- */
